84d60a96f0802ac0d399864055f300c3fff56f76
chengong <chengong@skieer.com>
2018-05-04 20:36:47 +0800

    注册页面调整

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const pkg = require('./package')
 // mongo 配置
 // mongod --logpath "D:\Program Files\MongoDB\data\log\mongodb.log" --logappend --dbpath "D:\Program Files\MongoDB\data\db" --directoryperdb --serviceName MongoDB --install
 const app = express()
+// 设置静态文件目录
+app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs') //设置模板引擎 ejs
 // 设置模板全局常量
@@ -18,15 +20,9 @@ app.locals.blog = {
   title: pkg.name,
   description: pkg.description
 }
-// 添加模板必需的三个变量
-app.use(function (req, res, next) {
-  res.locals.user = req.session.user
-  res.locals.success = req.flash('success').toString()
-  res.locals.error = req.flash('error').toString()
-  next()
-})
-// 设置静态文件目录
-app.use(express.static(path.join(__dirname, 'public')))
+
+// flash 中间件，用来显示通知
+app.use(flash())
 // session 中间件
 app.use(session({
   name: config.session.key, // 设置 cookie 中保存 session id 的字段名称
@@ -40,9 +36,14 @@ app.use(session({
     url: config.mongodb// mongodb 地址
   })
 }))
-// flash 中间件，用来显示通知
-app.use(flash())
-
+// 添加模板必需的三个变量
+app.use(function (req, res, next) {
+  res.locals.user = req.session.user
+  res.locals.success = req.flash('success').toString()
+  res.locals.error = req.flash('error').toString()
+  next()
+})
+// app.locals 上通常挂载常量信息（如博客名、描述、作者这种不会变的信息），res.locals 上通常挂载变量信息，即每次请求可能的值都不一样（如请求者信息，res.locals.user = req.session.user）。
 // 路由
 routes(app)
 
